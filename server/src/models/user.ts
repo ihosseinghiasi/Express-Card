f18ed4559d34a72e53212615056285cf428403ba
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2024-09-30 11:27:46 +0330

    User Model Updated

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -6,6 +6,14 @@ const userSchema = new mongoose.Schema ({
     email: {type: String, require: true},
     phoneNumber: {type: String, require: true},
     password: {type: String, require: true},
+    salt: {type: String, require: true}
 })
 
 export const userModel = mongoose.model('User',userSchema,'User')
+
+export const getUsers = () => { userModel.find() }
+export const getUserById = (id: String) => { userModel.findById({ id }) }
+export const deleteUserById = (id: string) => { userModel.findByIdAndDelete({ _id: id }) }
+export const updateUserById = (id: string, values: Record<string, any>) => { userModel.findByIdAndUpdate(id, values) }
+export const createUser = (values: Record<string, any>) =>
+    new userModel(values).save().then((user) => user.toObject())
