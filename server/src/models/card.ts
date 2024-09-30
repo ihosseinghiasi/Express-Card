668e233ba394042dc63c3de6319fdcdd5a2e437c
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2024-09-30 11:36:41 +0330

    Category Model Updated

--- server/src/models/card.ts
+++ server/src/models/card.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose"
+import { categoryModel } from "./category";
 
 const cardSchema = new mongoose.Schema({
   cardCategory: { type: String },
@@ -8,3 +9,10 @@ const cardSchema = new mongoose.Schema({
 });
 
 export const cardModel = mongoose.model("Card", cardSchema, "Card");
+
+export const getCategories = () => { categoryModel.find() }
+export const getCategoryById = (id: String) => { categoryModel.findById({ id }) }
+export const deleteCategoryById = (id: string) => { categoryModel.findByIdAndDelete({ _id: id }) }
+export const updateCategoryById = (id: string, values: Record<string, any>) => { categoryModel.findByIdAndUpdate(id, values) }
+export const createCategory = (values: Record<string, any>) =>
+    new categoryModel(values).save().then((Category) => Category.toObject())
