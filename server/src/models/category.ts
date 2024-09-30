cc9bc63132a77c59616f9193ec17124c805da7b1
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2024-09-30 11:41:15 +0330

    Category Model Updated

--- server/src/models/category.ts
+++ server/src/models/category.ts
@@ -7,4 +7,11 @@ const categorySchema = new mongoose.Schema({
     image: {type: String}
 })
 
-export const categoryModel = mongoose.model('Category',categorySchema,'Category')
+export const categoryModel = mongoose.model('Category', categorySchema, 'Category')
+
+export const getCategories = () => { categoryModel.find() }
+export const getCategoryById = (id: String) => { categoryModel.findById({ id }) }
+export const deleteCategoryById = (id: string) => { categoryModel.findByIdAndDelete({ _id: id }) }
+export const updateCategoryById = (id: string, values: Record<string, any>) => { categoryModel.findByIdAndUpdate(id, values) }
+export const createCategory = (values: Record<string, any>) =>
+    new categoryModel(values).save().then((Category) => Category.toObject())
