e808a03d469d77fd8830a3bcd309f829facb1b6a
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2024-09-30 11:44:04 +0330

    Email Model Updated

--- server/src/models/emailTemplate.ts
+++ server/src/models/emailTemplate.ts
@@ -6,3 +6,10 @@ const emailTemplateSchema = new mongoose.Schema({
 })
 
 export const emailModel =  mongoose.model('emailTemplate',emailTemplateSchema,'emailTemplate')
+
+export const getEmails = () => { emailModel.find() }
+export const getEmailById = (id: String) => { emailModel.findById({ id }) }
+export const deleteEmailById = (id: string) => { emailModel.findByIdAndDelete({ _id: id }) }
+export const updateEmailById = (id: string, values: Record<string, any>) => { emailModel.findByIdAndUpdate(id, values) }
+export const createEmail = (values: Record<string, any>) =>
+    new emailModel(values).save().then((Email) => Email.toObject())
