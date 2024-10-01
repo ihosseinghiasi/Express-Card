5a64a936edfadd634acd66bab08a4050bdfd6bbc
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2024-10-02 01:58:07 +0330

    Create User Repository Generic

--- server/src/interface/base.repository.ts
+++ server/src/interface/base.repository.ts
@@ -1,8 +1,8 @@
 export interface BaseRepository<T> {
   create(data: T): Promise<T>
-  findAll(): Promise<T>
+  findAll(): Promise<T[]>
   findById(id: string): Promise<T | null>
-  update(id : string): Promise<T | null>
+  update(id: string, data: T): Promise<T | null>
   delete(id: string): Promise<T | null>
   findAllPaginatedWithFilter(
     filter: any,
