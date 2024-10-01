0dff267a091116a06c7b2018f6681ba4f08c6ab0
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2024-10-01 23:25:05 +0330

    Create User Interface

---
+++ server/src/interface/user.interface.ts
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+export default interface IUser extends mongoose.Document {
+  id: string,
+  firstName: string,
+  lastName: string,
+  email: string,
+  phoneNumber: string,
+  password: string,
+  salt: string
+}
