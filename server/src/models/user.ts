09a5b69542f9ddd49d7e7690c23e702eb6f1b2a1
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2024-10-01 23:32:55 +0330

    Create User Model

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,4 +1,5 @@
 import mongoose from "mongoose"
+import IUser from "../interface/user.interface"
 
 const userSchema = new mongoose.Schema ({
     firstName: {type: String, require: true},
@@ -7,13 +8,11 @@ const userSchema = new mongoose.Schema ({
     phoneNumber: {type: String, require: true},
     password: {type: String, require: true},
     salt: {type: String, require: true}
-})
+},
+{
+    timestamps: true    
+}
+)
 
-export const userModel = mongoose.model('User',userSchema,'User')
-
-export const getUsers = () => { userModel.find() }
-export const getUserById = (id: String) => { userModel.findById({ id }) }
-export const deleteUserById = (id: string) => { userModel.findByIdAndDelete({ _id: id }) }
-export const updateUserById = (id: string, values: Record<string, any>) => { userModel.findByIdAndUpdate(id, values) }
-export const createUser = (values: Record<string, any>) =>
-    new userModel(values).save().then((user) => user.toObject())
+const User = mongoose.model<IUser>('User',userSchema,'User')
+export default User
