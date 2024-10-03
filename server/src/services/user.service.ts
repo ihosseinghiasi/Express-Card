99fd06bb93e3f9b0ba5db79d666a8c27093444ea
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2024-10-03 16:33:57 +0330

    Create User Service

---
+++ server/src/services/user.service.ts
@@ -0,0 +1,38 @@
+import IUser from "../interface/user.interface";
+import UserRepository from "../repository/user.repository";
+
+export default class userService {
+  private readonly UserRepository: UserRepository
+
+  constructor() {
+    this.UserRepository = new UserRepository()
+  }
+
+  async create(data: IUser): Promise<IUser> {
+    return this.UserRepository.create(data)
+  }
+
+  async findAll(): Promise<IUser[]> {
+    return this.UserRepository.findAll()
+  }
+
+  async findById(id: string): Promise<IUser | null> {
+    return this.UserRepository.findById(id)
+  }
+
+  async update(id: string, data: IUser): Promise<IUser | null> {
+    return this.UserRepository.update(id, data)
+  }
+
+  async delete(id: string): Promise<IUser | null> {
+    return this.UserRepository.delete(id)
+  }
+
+   async findByEmail(email: string): Promise<IUser | null> {
+    return this.UserRepository.findByEmail(email);
+  }
+
+  async findByName(name: string): Promise<IUser | null> {
+    return this.UserRepository.findByName(name);
+  }
+}
