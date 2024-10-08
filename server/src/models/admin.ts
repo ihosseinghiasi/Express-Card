8245093fc9ff19be39efc1b51bf71e2ee1a93a8b
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2024-10-08 22:24:01 +0330

    Update Admin Model

--- server/src/models/admin.ts
+++ server/src/models/admin.ts
@@ -1,23 +1,23 @@
-// import mongoose from "mongoose"
-// import IAdmin from "../interface/admin.interface"
+import mongoose from "mongoose"
+import IAdmin from "../interface/admin.interface"
 
-// const adminSchema = new mongoose.Schema({
-//     firstName: {type: String, required: true},
-//     lastName: {type: String, required: true},
-//     email: {type: String, required: true},
-//     password: { type: String, required: true },
-//     phoneNumber: {type: String, require: true},
-//     department: {type: String},
-//     isAdmin: {type: Boolean},
-//     isProduct: {type: Boolean},
-//     isCard: {type: Boolean},
-//     isEmail: {type: Boolean},
-//     isReport: {type: Boolean},
-//     isTicket: {type: Boolean},
-//     isCategory: {type: Boolean},
-//     isUser: {type: Boolean},
-//     isPayment: {type: Boolean},
-// })
+const adminSchema = new mongoose.Schema({
+    firstName: {type: String, required: true},
+    lastName: {type: String, required: true},
+    email: {type: String, required: true},
+    password: { type: String, required: true },
+    phoneNumber: {type: String, require: true},
+    department: {type: String},
+    isAdmin: {type: Boolean},
+    isProduct: {type: Boolean},
+    isCard: {type: Boolean},
+    isEmail: {type: Boolean},
+    isReport: {type: Boolean},
+    isTicket: {type: Boolean},
+    isCategory: {type: Boolean},
+    isUser: {type: Boolean},
+    isPayment: {type: Boolean},
+})
 
-// const Admin = mongoose.model<IAdmin>('Admin', adminSchema, 'Admin')
-// export default Admin
+const Admin = mongoose.model<IAdmin>('Admin', adminSchema, 'Admin')
+export default Admin
