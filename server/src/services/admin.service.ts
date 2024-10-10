d4cc2192894448a9f94e96c7e8602ea4e4bdf887
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2024-10-11 02:11:46 +0330

    Update Admin Service

--- server/src/services/admin.service.ts
+++ server/src/services/admin.service.ts
@@ -2,29 +2,29 @@ import IAdmin from "../interface/admin.interface";
 import AdminRepository from "../repository/admin.repository";
 
 export default class AdminService {
-  private readonly AdminRepository: AdminRepository
+  private readonly adminRepository: AdminRepository
 
   constructor() {
-    this.AdminRepository = new AdminRepository()
+    this.adminRepository = new AdminRepository()
   }
 
   async create(data: IAdmin): Promise<IAdmin> {
-    return this.AdminRepository.create(data)
+    return this.adminRepository.create(data)
   }
 
   async findAll(): Promise<IAdmin[] | null> {
-    return this.AdminRepository.findAll()
+    return this.adminRepository.findAll()
   }
 
   async findById(id: string): Promise<IAdmin | null> {
-    return this.AdminRepository.findById(id)
+    return this.adminRepository.findById(id)
   }
 
   async update(id: string, data: IAdmin): Promise<IAdmin | null> {
-    return this.AdminRepository.update(id, data)
+    return this.adminRepository.update(id, data)
   }
 
   async delete(id: string): Promise<IAdmin | null> {
-    return this.AdminRepository.delete(id)
+    return this.adminRepository.delete(id)
   }
 }
