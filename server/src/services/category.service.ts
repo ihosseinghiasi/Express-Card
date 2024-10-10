dd476705f871d814ba4f875f0d724721bd471feb
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2024-10-11 02:09:44 +0330

    Create Category Service

---
+++ server/src/services/category.service.ts
@@ -0,0 +1,30 @@
+import ICategory from "../interface/category.interface";
+import CategoryRepository from "../repository/category.repository";
+
+export default class CategoryService {
+  private readonly categoryRepository: CategoryRepository
+
+  constructor() {
+    this.categoryRepository = new CategoryRepository()
+  }
+
+  create(data: ICategory): Promise<ICategory> {
+    return this.categoryRepository.create(data)
+  }
+
+  async findAll(): Promise<ICategory[] | null> {
+    return this.categoryRepository.findAll()
+  }
+
+  async findById(id: string): Promise<ICategory | null> {
+    return this.categoryRepository.findById(id)
+  }
+
+  async update(id: string, data: ICategory): Promise<ICategory | null> {
+    return this.categoryRepository.update(id, data)
+  }
+
+  async delete(id: string): Promise<ICategory | null> {
+    return this.categoryRepository.delete(id)
+  }
+}
