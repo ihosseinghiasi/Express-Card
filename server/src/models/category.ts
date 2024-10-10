8d1992f140bfd94a103fada7149600304d5a92e2
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2024-10-11 01:25:32 +0330

    Update Category Model

--- server/src/models/category.ts
+++ server/src/models/category.ts
@@ -1,5 +1,5 @@
 import mongoose from "mongoose"
-
+import ICategory from "../interface/category.interface"
 const categorySchema = new mongoose.Schema({
     categoryName: {type: String},
     title: {type: String},
@@ -7,11 +7,5 @@ const categorySchema = new mongoose.Schema({
     image: {type: String}
 })
 
-export const categoryModel = mongoose.model('Category', categorySchema, 'Category')
-
-export const getCategories = () => { categoryModel.find() }
-export const getCategoryById = (id: String) => { categoryModel.findById({ id }) }
-export const deleteCategoryById = (id: string) => { categoryModel.findByIdAndDelete({ _id: id }) }
-export const updateCategoryById = (id: string, values: Record<string, any>) => { categoryModel.findByIdAndUpdate(id, values) }
-export const createCategory = (values: Record<string, any>) =>
-    new categoryModel(values).save().then((Category) => Category.toObject())
+const Category = mongoose.model<ICategory>('Category', categorySchema, 'Category')
+export default Category
