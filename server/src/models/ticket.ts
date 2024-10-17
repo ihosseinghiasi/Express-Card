8dcd484379d326ada758062cd34ddb9cfbf75082
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2024-10-17 21:37:20 +0330

    Create Ticket Model

--- server/src/models/ticket.ts
+++ server/src/models/ticket.ts
@@ -1,23 +1,17 @@
 import mongoose from "mongoose";
 
 const ticketSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  title: { type: String },
+  subject: { type: String },
   status: { type: String },
+  sender: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   targetDepartment: { type: String },
-  tickets: { type: Number, default: 0 },
-  adminTickets: { type: Number, default: 0 },
-  userTickets: { type: Number, default: 0 },
-  newUserTickets: { type: Number, default: 0 },
-  newAdminTickets: { type: Number, default: 0 },
-  ticket: {},
+  ticketNumbers: { type: Number, default: 0 },
+  userTicketsNumber: { type: Number, default: 0 },
+  targetTicketsNumber: { type: Number, default: 0 },
+  newUserTicketsNumber: { type: Number, default: 0 },
+  newTargetTicketsNumber: { type: Number, default: 0 },
+  tickets: {},
 });
 
-export const ticketModel = mongoose.model("Ticket", ticketSchema, "Ticket");
-
-export const getTickets = () => { ticketModel.find() }
-export const getTicketById = (id: String) => { ticketModel.findById({ id }) }
-export const deleteTicketById = (id: string) => { ticketModel.findByIdAndDelete({ _id: id }) }
-export const updateTicketById = (id: string, values: Record<string, any>) => { ticketModel.findByIdAndUpdate(id, values) }
-export const createTicket = (values: Record<string, any>) =>
-    new ticketModel(values).save().then((ticket) => ticket.toObject())
+const Ticket = mongoose.model("Ticket", ticketSchema, "Ticket");
+export default Ticket
