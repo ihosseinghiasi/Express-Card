c95593caa81a3e97a971050251de41dbf94b775d
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2025-02-05 21:45:39 +0330

    Create Ticket Service For User Panel .

---
+++ src/services/userPanel/ticket.service.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+export const addTicket = async (ticket) => {
+  return axios.post("http://locahost:4000/adminPanel/ticket/createTicket", {
+    ticket,
+  });
+};
+
+export const getTickets = async () => {
+  return await axios
+    .get("http://localhost:4000/adminPanel/ticket/getAllTickets")
+    .then((res) => {
+      return res;
+    });
+};
+
+export const getTicket = async (params) => {
+  return await axios
+    .get(`http://localhost:4000/adminPanel/ticket/getTicket/${params.id}`)
+    .then((res) => {
+      return res;
+    });
+};
+
+export const updateTicket = async (params, answer) => {
+  return await axios.put(
+    `http://localhost:4000/adminPanel/ticket/answerTicket/${params.id}`,
+    {
+      answer,
+    }
+  );
+};
+
+export const deleteTicket = async (id) => {
+  return await axios.delete(
+    `http://localhost:4000/adminPanel/ticket/deleteTicket/${id}`
+  );
+};
