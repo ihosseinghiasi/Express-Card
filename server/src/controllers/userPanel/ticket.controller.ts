61430a14f7b736a2f2ea13a34c403bf634bab9d9
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2025-02-09 16:52:10 +0330

    Refractory Add Ticket For User .

--- server/src/controllers/userPanel/ticket.controller.ts
+++ server/src/controllers/userPanel/ticket.controller.ts
@@ -15,38 +15,53 @@ export default class TicketController {
 
   async createTicket(req: Request, res: Response) {
     try {
-      const authenticatedId = localStorage.getItem("userAuthenticatedId");
-      if (authenticatedId) {
-        const user = await this.userService.findById(authenticatedId);
-        const tagIgnore = /(<([^>]+)>)/g;
-        const userFullName = `${user?.firstName} ${user?.lastName}`;
-        const data: ITicket = {
+      const data = await this.createTicketData(req, res);
+      const ticketContent = await this.createTicketContent(req, res, data);
+      data.tickets = ticketContent;
+      const ticket = await this.ticketService.create(data);
+      res.status(200).json(ticket);
+    } catch (error: unknown) {
+      throw new Error(error as string);
+    }
+  }
+
+  async createTicketContent(req: Request, res: Response, data: ITicket) {
+    let tickets = {};
+    const tagIgnore = /(<([^>]+)>)/g;
+    const newTicket = req.body.ticket.tickets.replace(tagIgnore, "");
+    tickets = {
+      ticket1: {
+        sender: data.sender,
+        text: newTicket,
+        date: persianDate,
+      },
+    };
+    return tickets;
+  }
+
+  async createTicketData(req: Request, res: Response) {
+    let data!: ITicket;
+    const authenticatedId = localStorage.getItem("userAuthenticatedId");
+    if (authenticatedId) {
+      const user = await this.userService.findById(authenticatedId);
+      if (user) {
+        const userFullName = `${user.firstName} ${user.lastName}`;
+        data = {
           subject: req.body.ticket.subject,
           status: "ارسال کاربر",
           targetDepartment: req.body.ticket.targetDepartment,
           tickets: {},
-          sender: userFullName || "",
-          senderId: user?._id || "",
+          sender: userFullName,
+          senderId: user._id,
           ticketNumbers: 1,
           userTicketsNumber: 1,
           targetTicketsNumber: 0,
           newUserTicketsNumber: 1,
           newTargetTicketsNumber: 0,
         };
-        const newTicket = req.body.ticket.tickets.replace(tagIgnore, "");
-        data.tickets = {
-          ticket1: {
-            sender: data.sender,
-            text: newTicket,
-            date: persianDate,
-          },
-        };
-        const ticket = await this.ticketService.create(data);
-        res.status(200).json(ticket);
       }
-    } catch (error: unknown) {
-      throw new Error(error as string);
     }
+    return data;
   }
 
   async getAllTickets(req: Request, res: Response) {
