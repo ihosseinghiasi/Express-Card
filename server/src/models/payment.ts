6abcca062c3fd7e6cdce9d43a0e8b27b5a27b1cc
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2025-02-10 13:57:18 +0330

    Create Payment Model .

---
+++ server/src/models/payment.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+import IPayment from "../interface/payment.interface";
+
+const paymentSchema = new mongoose.Schema({
+  count: { type: Number },
+  title: { type: String },
+  price: { type: Number },
+  totalPrice: { type: Number },
+  periodOfTime: { type: Number },
+  resnumber: { type: String },
+  payment: { type: Boolean, default: false },
+  isNewPaymentForAdmin: { type: Boolean, default: true },
+  isNewPaymentForUser: { type: Boolean, default: true },
+});
+
+const Payment = mongoose.model<IPayment>("Payment", paymentSchema, "Payment");
+export default Payment;
