d1eea3be7f10599efdba8ec22caeefe518dc1e60
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2025-02-10 22:08:02 +0330

    Create Payment Controller.

--- server/src/controllers/adminPanel/paymant.controller.ts
+++ server/src/controllers/adminPanel/paymant.controller.ts
@@ -1,15 +1,15 @@
-// import { Request, Response } from "express";
-// import IPayment from "../../interface/payment.interface";
-// import PaymentService from "../../services/adminPanel/payment.service";
+import { Request, Response } from "express";
+import IPayment from "../../interface/payment.interface";
+import PaymentService from "../../services/adminPanel/payment.service";
 
-// export default class Payment {
-//   private readonly paymentService: PaymentService;
+export default class Payment {
+  private readonly paymentService: PaymentService;
 
-//   constructor() {
-//     this.paymentService = new PaymentService();
-//   }
+  constructor() {
+    this.paymentService = new PaymentService();
+  }
 
-//   async pay(req: Request, res: Response) {
+  async pay(req: Request, res: Response) {
 
-//   }
-// }
+  }
+}
