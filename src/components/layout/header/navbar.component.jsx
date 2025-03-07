926ed7e0a96ac0eb7ad0e6287ecf64c00f591d43
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2025-03-07 21:33:15 +0330

    Update User Sidebar.

--- src/components/layout/header/navbar.component.jsx
+++ src/components/layout/header/navbar.component.jsx
@@ -5,52 +5,52 @@ import { jwtDecode } from "jwt-decode";
 import Cookies from "js-cookie";
 import "../../../css/shop/navbar.css";
 
-// const items = [
-//   {
-//     key: "1",
-//     label: (
-//       <a
-//         target="_blank"
-//         rel="noopener noreferrer"
-//         href="https://www.antgroup.com"
-//       >
-//         1st menu item
-//       </a>
-//     ),
-//   },
-//   {
-//     key: "2",
-//     label: (
-//       <a
-//         target="_blank"
-//         rel="noopener noreferrer"
-//         href="https://www.aliyun.com"
-//       >
-//         2nd menu item (disabled)
-//       </a>
-//     ),
-//     icon: <SmileOutlined />,
-//     disabled: true,
-//   },
-//   {
-//     key: "3",
-//     label: (
-//       <a
-//         target="_blank"
-//         rel="noopener noreferrer"
-//         href="https://www.luohanacademy.com"
-//       >
-//         3rd menu item (disabled)
-//       </a>
-//     ),
-//     disabled: true,
-//   },
-//   {
-//     key: "4",
-//     danger: true,
-//     label: "a danger item",
-//   },
-// ];
+const items = [
+  {
+    key: "1",
+    label: (
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.antgroup.com"
+      >
+        1st menu item
+      </a>
+    ),
+  },
+  {
+    key: "2",
+    label: (
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.aliyun.com"
+      >
+        2nd menu item (disabled)
+      </a>
+    ),
+    icon: "",
+    disabled: true,
+  },
+  {
+    key: "3",
+    label: (
+      <a
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://www.luohanacademy.com"
+      >
+        3rd menu item (disabled)
+      </a>
+    ),
+    disabled: true,
+  },
+  {
+    key: "4",
+    danger: true,
+    label: "a danger item",
+  },
+];
 
 const NavbarComponent = () => {
   const [person, setPerson] = useState("");
