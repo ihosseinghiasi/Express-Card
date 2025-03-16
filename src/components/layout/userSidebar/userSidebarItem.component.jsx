606268c20f916923ccdf061e6d6a7603f169ad78
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2025-03-16 19:41:38 +0330

    Update User Sidebar.

--- src/components/layout/userSidebar/userSidebarItem.component.jsx
+++ src/components/layout/userSidebar/userSidebarItem.component.jsx
@@ -5,17 +5,12 @@ import "../../../css/user/userSidebar.css";
 const SidebarItem = ({ item }) => {
   const [pageId, setPageId] = useState(localStorage.getItem("pageId"));
 
-  const getnew = () => {
-    // localStorage.setItem("pageId", "00113");
-    // const pid = localStorage.getItem("pageId");
-    // console.log(pid);
-  };
   return (
     <Link to={item.link}>
       <div
         className={`sidebar-item ${pageId === item.id ? "defaltSelected" : ""}`}
         style={{ color: "white" }}
-      >
+      > 
         <div className="sidebar-title">
           <img src={item.icon} alt="counter" className="ms-1" />
           <p>{item.title}</p>
