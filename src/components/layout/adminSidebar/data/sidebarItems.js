c69fd42fc1641e3698b67fe1eab500a586e1578a
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2025-04-13 13:51:36 +0330

    Update Admin Sidebar Items

--- src/components/layout/adminSidebar/data/sidebarItems.js
+++ src/components/layout/adminSidebar/data/sidebarItems.js
@@ -1,5 +1,6 @@
 export const items = [
   {
+    id: "00",
     title: "پیشخوان",
     icon: "/uploads/icons/home.svg",
     link: "/admin/counter",
@@ -7,32 +8,110 @@ export const items = [
   {
     title: " مدیران سایت",
     icon: "/uploads/icons/users.svg",
-    link: "/admin/allAdmins",
+    links: [
+      {
+        id: "11",
+        title: "ایجاد مدیر",
+        icon: "/uploads/icons/users.svg",
+        link: "/admin/newAdmin",
+      },
+      {
+        id: "12",
+        title: "مدیران",
+        icon: "/uploads/icons/users.svg",
+        link: "/admin/allAdmins",
+      },
+    ],
   },
   {
     title: " کاربران سایت",
     icon: "/uploads/icons/users.svg",
-    link: "/admin/allUsers",
+    links: [
+      {
+        id: "21",
+        title: "ایجاد مدیر",
+        icon: "/uploads/icons/users.svg",
+        link: "/admin/newAdmin",
+      },
+      {
+        id: "22",
+        title: "مدیران",
+        icon: "/uploads/icons/users.svg",
+        link: "/admin/allAdmins",
+      },
+    ],
   },
   {
     title: "دسته بندی ها",
     icon: "/uploads/icons/category.svg",
-    link: "/admin/allCategories",
+    links: [
+      {
+        id: "31",
+        title: "ایجاد مدیر",
+        icon: "/uploads/icons/users.svg",
+        link: "/admin/newAdmin",
+      },
+      {
+        id: "32",
+        title: "مدیران",
+        icon: "/uploads/icons/users.svg",
+        link: "/admin/allAdmins",
+      },
+    ],
   },
   {
     title: "محصولات ",
     icon: "/uploads/icons/product.svg",
-    link: "/admin/allProducts",
+    links: [
+      {
+        id: "41",
+        title: "ایجاد مدیر",
+        icon: "/uploads/icons/users.svg",
+        link: "/admin/newAdmin",
+      },
+      {
+        id: "42",
+        title: "مدیران",
+        icon: "/uploads/icons/users.svg",
+        link: "/admin/allAdmins",
+      },
+    ],
   },
   {
     title: "کارت ها ",
     icon: "/uploads/icons/credit-card.svg",
-    link: "/admin/allCards",
+    links: [
+      {
+        id: "51",
+        title: "ایجاد مدیر",
+        icon: "/uploads/icons/users.svg",
+        link: "/admin/newAdmin",
+      },
+      {
+        id: "52",
+        title: "مدیران",
+        icon: "/uploads/icons/users.svg",
+        link: "/admin/allAdmins",
+      },
+    ],
   },
   {
     title: "تیکت ها",
     icon: "/uploads/icons/message-square.svg",
-    link: "/admin/allTickets",
+    links: [
+      {
+        id: "61",
+        title: "ایجاد مدیر",
+        icon: "/uploads/icons/users.svg",
+        link: "/admin/newAdmin",
+      },
+      {
+        id: "62",
+        title: "مدیران",
+        icon: "/uploads/icons/users.svg",
+        link: "/admin/allAdmins",
+      },
+    ],
   },
   {
     title: "ایمیل ها",
