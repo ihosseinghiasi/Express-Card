e885d83ac19cd699bed89eb7e8c8d020844fbacd
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2025-04-24 17:50:55 +0330

    Create Email Controller.

---
+++ server/src/controllers/adminPanel/email.controller.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from "express";
+import IEmail from "../../interface/email.interface";
+import EmailService from "../../services/adminPanel/email.service";
+
+export default class EmailController {
+  private readonly emailService: EmailService;
+
+  constructor() {
+    this.emailService = new EmailService();
+  }
+
+  async create(req: Request, res: Response) {
+    try {
+      console.log("create email");
+    } catch (error: unknown) {
+      throw new Error(error as string);
+    }
+  }
+
+  async findAllAdmins(req: Request, res: Response) {
+    try {
+    } catch (error: unknown) {
+      throw new Error(error as string);
+    }
+  }
+
+  async findAdmin(req: Request, res: Response) {
+    try {
+    } catch (error: unknown) {
+      throw new Error(error as string);
+    }
+  }
+
+  async deleteAdmin(req: Request, res: Response) {
+    try {
+    } catch (error: unknown) {
+      throw new Error(error as string);
+    }
+  }
+}
