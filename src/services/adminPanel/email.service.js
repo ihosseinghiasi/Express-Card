938ec6cb172e29a7e1239c04806211b84256cc00
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2025-04-24 18:08:49 +0330

    Create Email Service.

---
+++ src/services/adminPanel/email.service.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+export const addEmail = async (email) => {
+  return axios.post(
+    `http://localhost:4000/adminPanel/email/createEmail`,
+    {
+      email,
+    }
+  );
+};
+
+export const getEmailTemplates = async () => {
+  return await axios
+    .get("http://localhost:4000/adminPanel/emailTemplate/getAllEmailTemplates")
+    .then((res) => {
+      return res;
+    });
+};
+
+export const getEmailTemplate = async (params) => {
+  return await axios
+    .get(
+      `http://localhost:4000/adminPanel/emailTemplate/getEmailTemplate/${params.id}`
+    )
+    .then((res) => {
+      return res;
+    });
+};
+
+export const updateEmailTemplate = async (params, email) => {
+  return await axios.put(
+    `http://localhost:4000/adminPanel/emailTemplate/updateEmailTemplate/${params.id}`,
+    { email }
+  );
+};
+
+export const deleteEmailTemplate = async (id) => {
+  return await axios.delete(
+    `http://localhost:4000/adminPanel/emailTemplate/deleteEmailtemplate/${id}`
+  );
+};
