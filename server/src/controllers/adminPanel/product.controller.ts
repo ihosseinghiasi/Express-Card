4d25d0331b8d039b7519af76add68e7b998fd0db
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2025-04-25 21:37:36 +0330

    Add Count Value In Add And Update Product.

--- server/src/controllers/adminPanel/product.controller.ts
+++ server/src/controllers/adminPanel/product.controller.ts
@@ -3,10 +3,10 @@ import ProductService from "../../services/adminPanel/product.service";
 import IProduct from "../../interface/product.interface";
 
 export default class ProductController {
-  private readonly productService: ProductService
+  private readonly productService: ProductService;
 
   constructor() {
-    this.productService = new ProductService()
+    this.productService = new ProductService();
   }
 
   async createProduct(req: Request, res: Response) {
@@ -17,66 +17,68 @@ export default class ProductController {
         categoryTitle: req.body.categoryTitle,
         cycle: req.body.cycle,
         price: req.body.price,
+        count: 0,
         description: req.body.description,
         accessible: req.body.accessible,
         image: req.file?.filename || "unimage.png",
-        fields: req.body.fields.split(','),
-      }
-      const product = await this.productService.create(data)
-      res.status(200).json(product)
+        fields: req.body.fields.split(","),
+      };
+      const product = await this.productService.create(data);
+      res.status(200).json(product);
     } catch (error: unknown) {
-      throw new Error(error as string)
+      throw new Error(error as string);
     }
   }
 
   async getAllProducts(req: Request, res: Response) {
-   try {
-    const products = await this.productService.findAll()
-    res.status(200).json(products)
-   } catch (error: unknown) {
-    throw new Error(error as string)
-   }
+    try {
+      const products = await this.productService.findAll();
+      res.status(200).json(products);
+    } catch (error: unknown) {
+      throw new Error(error as string);
+    }
   }
 
   async getProduct(req: Request, res: Response) {
     try {
-      const id: string = req.params.id
-      const product = await this.productService.findById(id)
-      res.status(200).json(product)
-   } catch (error: unknown) {
-      throw new Error(error as string)
-   }
+      const id: string = req.params.id;
+      const product = await this.productService.findById(id);
+      res.status(200).json(product);
+    } catch (error: unknown) {
+      throw new Error(error as string);
+    }
   }
 
   async updateProduct(req: Request, res: Response) {
     try {
-      const id: string = req.params.id
-      const oldProduct = await this.productService.findById(id)
+      const id: string = req.params.id;
+      const oldProduct = await this.productService.findById(id);
       const data: IProduct = {
-      productName: req.body.productName,
-      title: req.body.title,
-      categoryTitle: req.body.categoryTitle,
-      cycle: req.body.cycle,
-      price: req.body.price,
-      description: req.body.description,
-      accessible: req.body.accessible,
-      image: req.file?.filename || oldProduct?.image || "",
-      fields: req.body.fields.split(','),
-      }
-      const product = await this.productService.update(id, data)
-      res.status(200).json(product)
+        productName: req.body.productName,
+        title: req.body.title,
+        categoryTitle: req.body.categoryTitle,
+        cycle: req.body.cycle,
+        count: req.body.count,
+        price: req.body.price,
+        description: req.body.description,
+        accessible: req.body.accessible,
+        image: req.file?.filename || oldProduct?.image || "",
+        fields: req.body.fields.split(","),
+      };
+      const product = await this.productService.update(id, data);
+      res.status(200).json(product);
     } catch (error: unknown) {
-      throw new Error(error as string)
+      throw new Error(error as string);
     }
   }
 
   async deleteProduct(req: Request, res: Response) {
     try {
-      const id: string = req.params.id
-      const product = await this.productService.delete(id)
-      res.status(200).json(product)
+      const id: string = req.params.id;
+      const product = await this.productService.delete(id);
+      res.status(200).json(product);
     } catch (error: unknown) {
-      throw new Error(error as string)
+      throw new Error(error as string);
     }
   }
 }
