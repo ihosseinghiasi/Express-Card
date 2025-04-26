a04fce7e1b6c34c3021d53487bb543a614ef7f1c
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2025-04-26 14:32:48 +0330

    Creact Decrease Count For Product Of Card.

--- server/src/controllers/adminPanel/card.controller.ts
+++ server/src/controllers/adminPanel/card.controller.ts
@@ -33,6 +33,58 @@ export default class CardController {
     }
   }
 
+  async getAllCards(req: Request, res: Response) {
+    try {
+      const cards = await this.cardService.findAll();
+      res.status(200).json(cards);
+    } catch (error: unknown) {
+      throw new Error(error as string);
+    }
+  }
+
+  async getCard(req: Request, res: Response) {
+    try {
+      const id: string = req.params.id;
+      const card = await this.cardService.findById(id);
+      res.status(200).json(card);
+    } catch (error: unknown) {
+      throw new Error(error as string);
+    }
+  }
+
+  async updateCard(req: Request, res: Response) {
+    try {
+      const id: string = req.params.id;
+      const fieldValues = req.body.data.fieldValues;
+      const fieldNames = req.body.data.fieldNames;
+      const fields = this.setFields(fieldNames, fieldValues);
+
+      const data: ICard = {
+        cardCategory: req.body.data.card.cardCategory,
+        cardProduct: req.body.data.card.cardProduct,
+        cardStatus: req.body.data.card.cardStatus,
+        cardFields: fields,
+      };
+      const card = await this.cardService.update(id, data);
+      res.status(200).json(card);
+    } catch (error: unknown) {
+      throw new Error(error as string);
+    }
+  }
+
+  async deleteCard(req: Request, res: Response) {
+    try {
+      const id: string = req.params.id;
+      const card = await this.cardService.delete(id);
+      if (card) {
+        this.DecreaseProductCount(card.cardProduct);
+      }
+      res.status(200).json(card);
+    } catch (error: unknown) {
+      throw new Error(error as string);
+    }
+  }
+
   async setFields(fieldValues: string, fieldNames: string[]) {
     let fields = {};
     if (fieldValues !== "") {
@@ -73,57 +125,28 @@ export default class CardController {
     }
   }
 
-  async getAllCards(req: Request, res: Response) {
+  async DecreaseProductCount(productTitle: string) {
     try {
-      const cards = await this.cardService.findAll();
-      res.status(200).json(cards);
-    } catch (error: unknown) {
-      throw new Error(error as string);
-    }
-  }
-
-  async getCard(req: Request, res: Response) {
-    try {
-      const id: string = req.params.id;
-      const card = await this.cardService.findById(id);
-      res.status(200).json(card);
-    } catch (error: unknown) {
-      throw new Error(error as string);
-    }
-  }
-
-  async updateCard(req: Request, res: Response) {
-    try {
-      const id: string = req.params.id;
-      const fieldValues = req.body.data.fieldValues;
-      const fieldNames = req.body.data.fieldNames;
-      let fields = {};
-      if (fieldValues !== "") {
-        fields = Object.fromEntries(
-          fieldNames.map((fieldName: string, index: number) => [
-            `field${[index]}`,
-            { fieldName: fieldName, fieldValue: fieldValues[index] },
-          ])
-        );
+      const product = await this.productService.findByTitle(productTitle);
+      let productID: string = "";
+      if (product) {
+        Object.values(product).map((p) => {
+          productID = p._id;
+        });
+        const data: IProduct = {
+          productName: product.productName,
+          title: product.title,
+          categoryTitle: product.categoryTitle,
+          cycle: product.cycle,
+          count: --product.count,
+          price: product.price,
+          description: product.description,
+          accessible: product.accessible,
+          image: product.image,
+          fields: product.fields,
+        };
+        await this.productService.update(productID, data);
       }
-      const data: ICard = {
-        cardCategory: req.body.data.card.cardCategory,
-        cardProduct: req.body.data.card.cardProduct,
-        cardStatus: req.body.data.card.cardStatus,
-        cardFields: fields,
-      };
-      const card = await this.cardService.update(id, data);
-      res.status(200).json(card);
-    } catch (error: unknown) {
-      throw new Error(error as string);
-    }
-  }
-
-  async deleteCard(req: Request, res: Response) {
-    try {
-      const id: string = req.params.id;
-      const card = await this.cardService.delete(id);
-      res.status(200).json(card);
     } catch (error: unknown) {
       throw new Error(error as string);
     }
