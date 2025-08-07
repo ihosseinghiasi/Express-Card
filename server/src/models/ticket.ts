5163b2509483fde4e05880b83c6ac5ce6dbeaec3
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2025-08-07 15:49:06 +0330

    Ticket Model Validated.

--- server/src/models/ticket.ts
+++ server/src/models/ticket.ts
@@ -2,16 +2,16 @@ import mongoose from "mongoose";
 import ITicket from "../interface/ticket.interface";
 
 const ticketSchema = new mongoose.Schema({
-  subject: { type: String },
-  status: { type: String },
-  sender: { type: String },
-  senderId: { type: String },
-  targetDepartment: { type: String },
-  ticketNumbers: { type: Number, default: 0 },
-  userTicketsNumber: { type: Number, default: 0 },
-  targetTicketsNumber: { type: Number, default: 0 },
-  newUserTicketsNumber: { type: Number, default: 0 },
-  newTargetTicketsNumber: { type: Number, default: 0 },
+  subject: { type: String, required: true },
+  status: { type: String, required: true },
+  sender: { type: String, required: true },
+  senderId: { type: String, required: true },
+  targetDepartment: { type: String, required: true },
+  ticketNumbers: { type: Number, default: 0, required: true },
+  userTicketsNumber: { type: Number, default: 0, required: true },
+  targetTicketsNumber: { type: Number, default: 0, required: true },
+  newUserTicketsNumber: { type: Number, default: 0, required: true },
+  newTargetTicketsNumber: { type: Number, default: 0, required: true },
   tickets: {},
 });
 
