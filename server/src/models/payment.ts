c0fb9656995d9486b42dcc9fc2fb0d7f94d558f6
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2025-08-07 15:46:17 +0330

    Payment Model Validated.

--- server/src/models/payment.ts
+++ server/src/models/payment.ts
@@ -2,12 +2,12 @@ import mongoose from "mongoose";
 import IPayment from "../interface/payment.interface";
 
 const paymentSchema = new mongoose.Schema({
-  userFullName: { type: String },
-  userId: { type: String },
-  title: { type: String },
-  price: { type: Number },
-  count: { type: Number },
-  totalPrice: { type: Number },
+  userFullName: { type: String, required: true },
+  userId: { type: String, required: true },
+  title: { type: String, required: true },
+  price: { type: Number, required: true },
+  count: { type: Number, required: true },
+  totalPrice: { type: Number, required: true },
 });
 
 const Payment = mongoose.model<IPayment>("Payment", paymentSchema, "Payment");
