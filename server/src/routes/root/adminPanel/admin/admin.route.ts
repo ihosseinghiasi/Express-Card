0f18e2ece237068a81b16ddb05d9aa2e09d8f744
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2025-08-07 15:37:03 +0330

    Edit Admin Route.

--- server/src/routes/root/adminPanel/admin/admin.route.ts
+++ server/src/routes/root/adminPanel/admin/admin.route.ts
@@ -2,22 +2,37 @@ import { Router } from "express";
 import AdminController from "../../../../controllers/adminPanel/admin.controller";
 
 class AdminRoute {
-  private readonly adminController: AdminController
-  public readonly router: Router
+  private readonly adminController: AdminController;
+  public readonly router: Router;
 
   constructor() {
-    this.adminController = new AdminController()
-    this.router = Router()
-    this.initRoutes()
+    this.adminController = new AdminController();
+    this.router = Router();
+    this.initRoutes();
   }
 
   private initRoutes() {
-    this.router.post("/createAdmin", this.adminController.createAdmin.bind(this.adminController))
-    this.router.get("/getAllAdmins", this.adminController.findAllAdmins.bind(this.adminController))
-    this.router.get("/getAdmin/:id", this.adminController.findAdmin.bind(this.adminController))
-    this.router.put("/updateAdmin/:id", this.adminController.updateAdmin.bind(this.adminController))
-    this.router.delete("/deleteAdmin/:id", this.adminController.deleteAdmin.bind(this.adminController))
+    this.router.post(
+      "/createAdmin",
+      this.adminController.createAdmin.bind(this.adminController)
+    );
+    this.router.get(
+      "/getAllAdmins",
+      this.adminController.findAllAdmins.bind(this.adminController)
+    );
+    this.router.get(
+      "/getAdmin/:id",
+      this.adminController.findAdmin.bind(this.adminController)
+    );
+    this.router.put(
+      "/updateAdmin/:id",
+      this.adminController.updateAdmin.bind(this.adminController)
+    );
+    this.router.delete(
+      "/deleteAdmin/:id",
+      this.adminController.deleteAdmin.bind(this.adminController)
+    );
   }
 }
 
-export default new AdminRoute().router
+export default new AdminRoute().router;
