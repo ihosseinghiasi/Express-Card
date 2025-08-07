8da642edf5e01968f3dadc97a1fe136273f267d7
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2025-08-07 15:42:23 +0330

    Card Model Validated.

--- server/src/models/card.ts
+++ server/src/models/card.ts
@@ -1,12 +1,12 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 import ICard from "../interface/card.interface";
 
 const cardSchema = new mongoose.Schema({
-  cardCategory: { type: String },
-  cardProduct: { type: String },
-  cardStatus: { type: String },
+  cardCategory: { type: String, required: true },
+  cardProduct: { type: String, required: true },
+  cardStatus: { type: String, required: true },
   cardFields: {},
 });
 
 const Card = mongoose.model<ICard>("Card", cardSchema, "Card");
-export default Card
+export default Card;
