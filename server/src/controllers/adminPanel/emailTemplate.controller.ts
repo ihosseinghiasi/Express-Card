5779de2dcf1e0b171dfc42adb03eb8e63bcad63e
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2025-08-16 13:37:34 +0330

    Error Handeling Of Email Methods.

--- server/src/controllers/adminPanel/emailTemplate.controller.ts
+++ server/src/controllers/adminPanel/emailTemplate.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import EmailTemplateService from "../../services/adminPanel/emailTemplate.service";
 import IEmailTemplate from "../../interface/emailTemplate.interface";
+import response from "../../config/response";
 
 export default class EmailTemplate {
   private readonly emailTemplateService: EmailTemplateService;
@@ -13,7 +14,10 @@ export default class EmailTemplate {
     try {
       const data: IEmailTemplate = req.body.email;
       const email = await this.emailTemplateService.create(data);
-      res.status(200).json(email);
+      if (!email) {
+        return response(res, 400, "Email Template Not Successfuly Created.");
+      }
+      return response(res, 201, "Email Template Successfuly Created.", email);
     } catch (error: unknown) {
       throw new Error(error as string);
     }
@@ -22,7 +26,10 @@ export default class EmailTemplate {
   async findAllEmailTemplates(req: Request, res: Response) {
     try {
       const emails = await this.emailTemplateService.findAll();
-      res.status(200).json(emails);
+      if (!emails) {
+        return response(res, 400, "Email Template Not Successfuly Finded.");
+      }
+      return response(res, 200, "Email Template Successfuly Created.", emails);
     } catch (error: unknown) {
       throw new Error(error as string);
     }
@@ -32,7 +39,10 @@ export default class EmailTemplate {
     try {
       const id: string = req.params.id;
       const email = await this.emailTemplateService.findOne(id);
-      res.status(200).json(email);
+      if (!email) {
+        return response(res, 404, "Email Template Not Successfuly finded.");
+      }
+      return response(res, 200, "Email Template Successfuly Finded.", email);
     } catch (error: unknown) {
       throw new Error(error as string);
     }
@@ -42,8 +52,11 @@ export default class EmailTemplate {
     try {
       const id: string = req.params.id;
       const data: IEmailTemplate = req.body.email;
-      const emailUpdated = await this.emailTemplateService.update(id, data);
-      res.status(200).json(emailUpdated);
+      const email = await this.emailTemplateService.update(id, data);
+      if (!email) {
+        return response(res, 400, "Email Template Not Successfuly Updated.");
+      }
+      return response(res, 200, "Email Template Successfuly Updated.", email);
     } catch (error: unknown) {
       throw new Error(error as string);
     }
@@ -53,7 +66,10 @@ export default class EmailTemplate {
     try {
       const id: string = req.params.id;
       const email = await this.emailTemplateService.delete(id);
-      res.status(200).json(email);
+      if (!email) {
+        return response(res, 400, "Email Template Not Successfuly Deleted.");
+      }
+      return response(res, 200, "Email Template Successfuly Deleted.", email);
     } catch (error: unknown) {
       throw new Error(error as string);
     }
