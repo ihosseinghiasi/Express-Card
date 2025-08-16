a906ba3c91aa1cf13a521b387629d73354792930
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2025-08-16 16:54:14 +0330

    Error Handeling Of Authentication Methods.

--- server/src/controllers/authentication/authentication.controller.ts
+++ server/src/controllers/authentication/authentication.controller.ts
@@ -9,6 +9,7 @@ import { createToken } from "../../middlewares/createToken";
 import { LocalStorage } from "node-localstorage";
 import emailSender from "../../config/email";
 import { replaceEmailTemplatePatterns } from "../../config/replaceEmailTemplatePattern";
+import response from "../../config/response";
 global.localStorage = new LocalStorage("./scratch");
 const { Smsir } = require("smsir-js");
 export default class UserAuthentication {
@@ -27,27 +28,35 @@ export default class UserAuthentication {
   async register(req: Request, res: Response) {
     try {
       const data: IUser = req.body.data;
+      const userCreated = await this.userService.findById(data._id);
+      if (userCreated) {
+        return response(res, 400, "This User Already Registerd.");
+      }
       data.phoneNumber = this._phoneNumber;
       const salt = await bcrypt.genSalt();
       data.password = await bcrypt.hash(data.password, salt);
       const user = await this.userService.create(data);
+      if (!user) {
+        return response(res, 400, "User Not Successfuly Created.");
+      }
       const fullName = `${user.firstName} ${user.lastName}`;
       const emailTemplate = await this.emailTemplateService.findOne(
         "679136dd9b82810ff9e5294c"
       );
-      if (emailTemplate) {
-        const emailPatterns = await replaceEmailTemplatePatterns(
-          fullName,
-          emailTemplate,
-          []
-        );
-        emailSender(
-          user.email,
-          emailPatterns.emailSubject,
-          emailPatterns.emailDescription
-        );
+      if (!emailTemplate) {
+        return response(res, 400, "Email Template Not Successfuly Find.");
       }
-      res.status(201).json(user);
+      const emailPatterns = await replaceEmailTemplatePatterns(
+        fullName,
+        emailTemplate,
+        []
+      );
+      emailSender(
+        user.email,
+        emailPatterns.emailSubject,
+        emailPatterns.emailDescription
+      );
+      return response(res, 201, "User Successfuly Registed.", user);
     } catch (error: unknown) {
       throw new Error(error as string);
     }
@@ -57,21 +66,33 @@ export default class UserAuthentication {
     try {
       const { email, password } = req.body.data;
       const user: IUser | null = await this.userService.login(email);
+      if (!user) {
+        return response(res, 400, "Invalid Email And/Or Password.");
+      }
       if (user) {
         const authentication = await bcrypt.compare(password, user.password);
         if (authentication) {
           const token = createToken(user._id);
           localStorage.setItem("userAuthenticatedId", user._id);
-          res.status(201).json({ person: user, token });
+          return response(res, 200, "User Successfuly Logged In", {
+            token,
+            person: user,
+          });
         }
       } else {
         const admin: IAdmin | null = await this.adminService.login(email);
+        if (!admin) {
+          return response(res, 400, "Invalid Email And/Or Password.");
+        }
         if (admin) {
           const authentication = await bcrypt.compare(password, admin.password);
           if (authentication) {
             const token = createToken(admin._id);
             localStorage.setItem("adminAuthenticatedId", admin._id);
-            res.status(201).json({ person: admin, token });
+            return response(res, 200, "Admin Successfuly Logged In", {
+              token,
+              person: admin,
+            });
           }
         }
       }
