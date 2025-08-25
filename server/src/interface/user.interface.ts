aad6d17784dac560887a0e0a2f53ca41136e014d
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2025-08-25 12:40:29 +0330

    Change User Interface.

--- server/src/interface/user.interface.ts
+++ server/src/interface/user.interface.ts
@@ -1,8 +1,19 @@
 export default interface IUser {
-  _id: string;
+  _id?: string;
   firstName: string;
   lastName: string;
   email: string;
   phoneNumber: string;
   password: string;
+  isAdmin: Boolean;
+  department: Boolean;
+  permissionAdmin: Boolean;
+  permissionProduct: Boolean;
+  permissionCard: Boolean;
+  permissionEmail: Boolean;
+  permissionReport: Boolean;
+  permissionTicket: Boolean;
+  permissionCategory: Boolean;
+  permissionUser: Boolean;
+  permissionPayment: Boolean;
 }
