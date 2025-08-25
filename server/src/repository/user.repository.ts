4769efa11121f8185d5598dc81ee0f39ebeb4bca
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2025-08-25 17:10:22 +0330

    Update Login Method.

--- server/src/repository/user.repository.ts
+++ server/src/repository/user.repository.ts
@@ -1,13 +1,13 @@
 import User from "../models/user";
 import IUser from "../interface/user.interface";
-import GenricRepository from "./generic.repository"
+import GenricRepository from "./generic.repository";
 
 export default class UserRepository extends GenricRepository<IUser> {
   constructor() {
-    super(User)
+    super(User);
   }
 
   async login(email: string): Promise<IUser | null> {
-    return User.findOne({email})
+    return User.findOne({ email });
   }
 }
