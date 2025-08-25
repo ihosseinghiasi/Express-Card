21e29e110d433b76d58f69522cb1eb994fc49f25
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2025-08-25 12:44:04 +0330

    Change User Model.

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -7,6 +7,17 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true },
   phoneNumber: { type: String, required: true },
   password: { type: String, required: true },
+  isAdmin: { type: Boolean, default: true },
+  department: { type: String, required: true },
+  permissionAdmin: { type: Boolean, default: false },
+  permissionProduct: { type: Boolean, default: false },
+  permissionCard: { type: Boolean, default: false },
+  permissionEmail: { type: Boolean, default: false },
+  permissionReport: { type: Boolean, default: false },
+  permissionTicket: { type: Boolean, default: false },
+  permissionCategory: { type: Boolean, default: false },
+  permissionUser: { type: Boolean, default: false },
+  permissionPayment: { type: Boolean, default: false },
 });
 
 const User = mongoose.model<IUser>("User", userSchema, "User");
