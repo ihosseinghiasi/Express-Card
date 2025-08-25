69da1bd5a1a3029814eaa3e7e8d916cacf22e5e6
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2025-08-25 12:56:10 +0330

    Change User Service Name To Constructor.

--- server/src/services/adminPanel/user.service.ts
+++ server/src/services/adminPanel/user.service.ts
@@ -1,35 +1,34 @@
 import IUser from "../../interface/user.interface";
 import UserRepository from "../../repository/user.repository";
 
-export default class userService {
-  private readonly userRepository: UserRepository
+export default class UserService {
+  private readonly userRepository: UserRepository;
 
   constructor() {
-    this.userRepository = new UserRepository()
+    this.userRepository = new UserRepository();
   }
 
   async create(data: IUser): Promise<IUser> {
-    return this.userRepository.create(data)
+    return this.userRepository.create(data);
   }
 
   async findAll(): Promise<IUser[] | null> {
-    return this.userRepository.findAll()
+    return this.userRepository.findAll();
   }
 
   async findById(id: string): Promise<IUser | null> {
-    return this.userRepository.findById(id)
+    return this.userRepository.findById(id);
   }
 
   async update(id: string, data: IUser): Promise<IUser | null> {
-    return this.userRepository.update(id, data)
+    return this.userRepository.update(id, data);
   }
 
   async delete(id: string): Promise<IUser | null> {
-    return this.userRepository.delete(id)
+    return this.userRepository.delete(id);
   }
 
   async login(email: string): Promise<IUser | null> {
-    return this.userRepository.login(email)
+    return this.userRepository.login(email);
   }
-
 }
