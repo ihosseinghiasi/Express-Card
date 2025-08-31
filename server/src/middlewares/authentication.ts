d8e818e6a1e31f29552708e983b3d6b32e5355f7
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2025-09-01 02:17:26 +0330

    Insert User Data In Request.

--- server/src/middlewares/authentication.ts
+++ server/src/middlewares/authentication.ts
@@ -1,21 +1,28 @@
-import { Request, Response, NextFunction } from "express";
+import { Response, NextFunction } from "express";
 import User from "../models/user";
 import jwt from "jsonwebtoken";
+import JwtPayload from "../interface/jwtPayload.interface";
 import response from "../config/response";
+import { AuthRequest } from "../config/namespase";
 
-const isLoggined = async (req: Request, res: Response, next: NextFunction) => {
-  const token = `req.header("x-auth-token")`;
-  if (!token) {
-    return response(res, 401, "Access Denied !");
-  }
-
+const isLoggined = async (
+  req: AuthRequest,
+  res: Response,
+  next: NextFunction
+) => {
   try {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      return response(res, 401, "Access Denied !");
+    }
     const jwt_key = process.env.JWT_KEY;
     if (!jwt_key) {
       response(res, 401, "Jwt Key Not Found !");
     }
-    const decodedToken = jwt.verify(token, jwt_key!);
-    const user = await User.findById(decodedToken);
+
+    const decodedToken = jwt.verify(token, jwt_key!) as JwtPayload;
+
+    const user = await User.findById(decodedToken.id);
     if (!user) {
       return response(res, 404, "User Not Finded.");
     }
@@ -25,3 +32,5 @@ const isLoggined = async (req: Request, res: Response, next: NextFunction) => {
     return response(res, 400, "Invalid Token");
   }
 };
+
+export default isLoggined;
