82c8b55290e5d27c7a5112a52e691167dfba7da7
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2025-09-01 02:15:54 +0330

    Add User Namespace.

--- server/src/config/namespase.d.ts
+++ server/src/config/namespase.d.ts
@@ -1,9 +1,6 @@
+import { Request } from "express";
 import IUser from "../interface/user.interface";
 
-declare global {
-  namespace Express {
-    interface Request {
-      user?: IUser;
-    }
-  }
+export interface AuthRequest extends Request {
+  user?: IUser;
 }
