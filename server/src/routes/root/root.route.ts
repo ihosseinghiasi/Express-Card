cb436d45e53ea7455cfab01d42dcbbe7ce2cafa3
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2025-09-04 18:13:12 +0330

    Add Dashboard Route In Index Route.

--- server/src/routes/root/root.route.ts
+++ server/src/routes/root/root.route.ts
@@ -3,9 +3,11 @@ import authentication from "./authentication/authentication.route";
 import persianDate from "./persianDate.route";
 import adminPanel from "./adminPanel/adminPanel.route";
 import userPanel from "./userPanel/userPanel.route";
+import dashboard from "./dashboard.route";
 
 const router = express.Router();
 
+router.use("/", dashboard);
 router.use("/authentication", authentication);
 router.use("/persianDate", persianDate);
 router.use("/adminPanel", adminPanel);
