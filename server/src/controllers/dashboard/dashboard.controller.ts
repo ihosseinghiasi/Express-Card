b2fb7a005e5faada9b1883db9d9824a0b97477fd
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2025-09-05 09:21:20 +0330

    User Response Function For Send Response.

--- server/src/controllers/dashboard/dashboard.controller.ts
+++ server/src/controllers/dashboard/dashboard.controller.ts
@@ -1,13 +1,12 @@
-import { Response } from "express";
+import { Request, Response } from "express";
 // import _ from lodash
-import { AuthRequest } from "../../config/namespase";
 import response from "../../config/response";
 
-const me = (req: AuthRequest, res: Response) => {
-  if (req.user) {
-    const user = req.user;
-    return response(res, 200, "Authenticated User Finded .", user);
+export default class Dashboard {
+  me(req: Request, res: Response) {
+    if (!req.user) {
+      return response(res, 404, "Authenticated User Not Finded.");
+    }
+    return response(res, 200, "Authenticated User Finded.", req.user);
   }
-};
-
-export default me;
+}
