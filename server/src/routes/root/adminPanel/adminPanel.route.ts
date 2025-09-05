5ac898e170c21f4d888553ee4c788f3df74144b0
ihosseinghiasi <hosseinghiasi.dev@gmail.com>
2025-09-05 09:25:48 +0330

    Remove isLoggined Moddleware From This Routes.

--- server/src/routes/root/adminPanel/adminPanel.route.ts
+++ server/src/routes/root/adminPanel/adminPanel.route.ts
@@ -9,12 +9,11 @@ import ticket from "./ticket/ticket.route";
 import emailTemplate from "./emailTemplate/emailTemplate.route";
 import email from "./email/email.route";
 import payment from "./payment/payment.route";
-import isLoggined from "../../../middlewares/authentication";
 
 const router = express.Router();
 
-router.use("/admin", isLoggined, admin);
-router.use("/user", isLoggined, user);
+router.use("/admin", admin);
+router.use("/user", user);
 router.use("/category", category);
 router.use("/product", product);
 router.use("/card", card);
